d03ec9aa7c34004177d9b158edbade3b3001b97a
Aryan Sethi <aryan.rdps@gmail.com>
2021-09-20 14:52:19 +0530

    Added SVG and favicon

--- src/App.js
+++ src/App.js
@@ -1,33 +1,97 @@
-import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import React, { useState } from 'react'
+import Alert from "./components/Alert";
 
-
-
-function App() {
-  return (
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
     
-    <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div className="container-fluid">
-			<a className="navbar-brand" href="/">TextUtils</a>
-			<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span className="navbar-toggler-icon"></span>
-			</button>
-			<div className="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-				<li className="nav-item">
-				<a className="nav-link active" aria-current="page" href="/">Home</a>
-				</li>
-				
-			</ul>
-			<form className="d-flex">
-				<input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-				<button className="btn btn-outline-success" type="submit">Search</button>
-			</form>
-			</div>
-		</div>
-	</nav>
-    </>
-  );
+  } from "react-router-dom";
+  
+
+
+
+// Clean App
+function App() {
+    
+    const [mode, setMode] = useState('light');
+    const [alert, setAlert] = useState(null);
+
+    const showAlert = (message , type)=>{
+        setAlert({
+            message: message,
+            type: type
+        });
+        setTimeout(()=>{
+            setAlert(null)
+        },1500);
+    };
+
+    const toggleMode = ()=>{
+        if (mode === 'light'){
+            setMode('dark');
+            document.body.style.backgroundColor = 'gray';
+            showAlert("Dark Mode has Been enabled" , "success");
+            document.title = "Text Utils - Dark Mode";
+        }
+        else{
+            setMode('light');
+            document.body.style.backgroundColor = 'white';
+            showAlert("Light Mode has Been enabled" , "success");
+            document.title = "Text Utils - Light Mode";
+        }
+    }
+
+    const nightmareMode = ()=>{
+
+
+        if (mode === 'light' || mode === 'dark'){
+            setMode('red');
+            document.body.style.backgroundColor = 'gray';
+            showAlert("NightMare Mode enabled" , "success");
+            
+        }
+        
+        else{
+            setMode('light');
+            document.body.style.backgroundColor = 'white';
+            showAlert("Light Mode has Been enabled" , "success");
+        
+        }
+    }
+    
+    return (
+        <>
+
+        {/* Exact Route vs w/o it
+        
+            /users --> comp1
+            /users/home --> comp2
+            they will be same w/o exact
+         */}
+        <Router>
+            {/* Passing Props */}
+            <Navbar logo="TextUtils" aboutName="About TU" mode={mode} toggleMode = {toggleMode} nightmareMode={nightmareMode}/>
+            <Alert alert={alert}/>
+
+            <div className="container my-5">
+                <Switch>
+                    <Route exact path="/about">
+                        <About mode={mode} />
+                    </Route>
+                    <Route exact path="/">
+                        <TextForm showAlert={showAlert} heading="Try Text Utils - Word counter , Character counter , remove extra spaces , copy , LowerCase , UpperCase" mode={mode}/>
+                    </Route>
+                </Switch>
+            
+
+            </div>      
+        </Router>
+        </> 
+    );
 }
 
 export default App;
